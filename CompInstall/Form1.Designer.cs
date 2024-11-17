197eb39507f1275413f5ba4775b4a970fcd2666e
HexsittDev <rafael074612@gmail.com>
2024-11-17 06:20:12 -0300

    Add files via upload

---
+++ CompInstall/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CompInstall
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.btnInstallBoth = new System.Windows.Forms.Button();
+            this.PBMain = new System.Windows.Forms.ProgressBar();
+            this.btnInstallx86 = new System.Windows.Forms.Button();
+            this.btnInstallx64 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInstallBoth
+            // 
+            this.btnInstallBoth.BackColor = System.Drawing.SystemColors.Control;
+            this.btnInstallBoth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstallBoth.Location = new System.Drawing.Point(150, 66);
+            this.btnInstallBoth.Name = "btnInstallBoth";
+            this.btnInstallBoth.Size = new System.Drawing.Size(229, 39);
+            this.btnInstallBoth.TabIndex = 0;
+            this.btnInstallBoth.Text = "Install Both";
+            this.btnInstallBoth.UseVisualStyleBackColor = false;
+            this.btnInstallBoth.Click += new System.EventHandler(this.btnInstallBoth_Click);
+            // 
+            // PBMain
+            // 
+            this.PBMain.Location = new System.Drawing.Point(12, 12);
+            this.PBMain.Name = "PBMain";
+            this.PBMain.Size = new System.Drawing.Size(504, 48);
+            this.PBMain.TabIndex = 1;
+            // 
+            // btnInstallx86
+            // 
+            this.btnInstallx86.BackColor = System.Drawing.SystemColors.Control;
+            this.btnInstallx86.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstallx86.Location = new System.Drawing.Point(12, 66);
+            this.btnInstallx86.Name = "btnInstallx86";
+            this.btnInstallx86.Size = new System.Drawing.Size(132, 39);
+            this.btnInstallx86.TabIndex = 2;
+            this.btnInstallx86.Text = "Install x86";
+            this.btnInstallx86.UseVisualStyleBackColor = false;
+            this.btnInstallx86.Click += new System.EventHandler(this.btnInstallx86_Click);
+            // 
+            // btnInstallx64
+            // 
+            this.btnInstallx64.BackColor = System.Drawing.SystemColors.Control;
+            this.btnInstallx64.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstallx64.Location = new System.Drawing.Point(385, 66);
+            this.btnInstallx64.Name = "btnInstallx64";
+            this.btnInstallx64.Size = new System.Drawing.Size(132, 39);
+            this.btnInstallx64.TabIndex = 3;
+            this.btnInstallx64.Text = "Install x64";
+            this.btnInstallx64.UseVisualStyleBackColor = false;
+            this.btnInstallx64.Click += new System.EventHandler(this.btnInstallx64_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(528, 114);
+            this.Controls.Add(this.btnInstallx64);
+            this.Controls.Add(this.btnInstallx86);
+            this.Controls.Add(this.PBMain);
+            this.Controls.Add(this.btnInstallBoth);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hexsitt Component Installer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInstallBoth;
+        private System.Windows.Forms.ProgressBar PBMain;
+        private System.Windows.Forms.Button btnInstallx86;
+        private System.Windows.Forms.Button btnInstallx64;
+    }
+}
+
---
+++ CompInstall/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompInstall
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInstallBoth_Click(object sender, EventArgs e)
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "CompInstall");
+            Directory.CreateDirectory(tempPath);
+
+            var installFiles = new (string fileName, string arguments)[]
+            {
+        ("2005x86.EXE", "/q"),
+        ("2005x64.EXE", "/q"),
+        ("2008x86.exe", "/q"),
+        ("2008x64.exe", "/q"),
+        ("2010x86.exe", "/quiet /norestart"),
+        ("2010x64.exe", "/quiet /norestart"),
+        ("2012x86.exe", "/quiet /norestart"),
+        ("2012x64.exe", "/quiet /norestart"),
+        ("2013x86.exe", "/quiet /norestart"),
+        ("2013x64.exe", "/quiet /norestart"),
+        ("2022x86.exe", "/quiet /norestart"),
+        ("2022x64.exe", "/quiet /norestart"),
+        ("xnafx40_redist.msi", "/quiet /norestart")
+            };
+
+            PBMain.Maximum = installFiles.Length;
+
+            try
+            {
+                foreach (var (fileName, arguments) in installFiles)
+                {
+                    string filePath = Path.Combine(tempPath, fileName);
+
+                    // Extract the embedded file to the temporary directory
+                    using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"CompInstall.{fileName}"))
+                    using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                        resourceStream?.CopyTo(fileStream);
+
+                    // Run the installation
+                    var processStartInfo = new ProcessStartInfo(
+                        fileName.EndsWith(".msi") ? "msiexec.exe" : filePath,
+                        fileName.EndsWith(".msi") ? $"/i \"{filePath}\" {arguments}" : arguments)
+                    {
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                    };
+
+                    using (var process = Process.Start(processStartInfo))
+                    {
+                        process?.WaitForExit();
+                    }
+
+                    // Update the progress bar
+                    PBMain.Value++;
+                }
+
+                // Delete all temporary files after installing all packages
+                Directory.Delete(tempPath, true); // Deletes the folder and its contents
+            }
+            catch (Exception ex)
+            {
+                // Display error message
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Clean up temporary files if there's an issue
+                try
+                {
+                    if (Directory.Exists(tempPath))
+                    {
+                        Directory.Delete(tempPath, true); // Deletes the folder and its contents
+                    }
+                }
+                catch
+                {
+                    // Do nothing if there's an error deleting temporary files
+                }
+
+                // Close the application after the error
+                Application.Exit();
+            }
+
+            // Display completion message after all installations and before closing the program
+            MessageBox.Show("All installations have been successfully completed!", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Close the application
+            Application.Exit(); // Closes the entire application
+        }
+
+        private void btnInstallx86_Click(object sender, EventArgs e)
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "CompInstall");
+            Directory.CreateDirectory(tempPath);
+
+            var installFiles = new (string fileName, string arguments)[]
+            {
+        ("2005x86.EXE", "/q"),
+        ("2008x86.exe", "/q"),
+        ("2010x86.exe", "/quiet /norestart"),
+        ("2012x86.exe", "/quiet /norestart"),
+        ("2013x86.exe", "/quiet /norestart"),
+        ("2022x86.exe", "/quiet /norestart"),
+        ("xnafx40_redist.msi", "/quiet /norestart")
+            };
+
+            PBMain.Maximum = installFiles.Length;
+
+            try
+            {
+                foreach (var (fileName, arguments) in installFiles)
+                {
+                    string filePath = Path.Combine(tempPath, fileName);
+
+                    // Extract the embedded file to the temporary directory
+                    using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"CompInstall.{fileName}"))
+                    using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                        resourceStream?.CopyTo(fileStream);
+
+                    // Run the installation
+                    var processStartInfo = new ProcessStartInfo(
+                        fileName.EndsWith(".msi") ? "msiexec.exe" : filePath,
+                        fileName.EndsWith(".msi") ? $"/i \"{filePath}\" {arguments}" : arguments)
+                    {
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                    };
+
+                    using (var process = Process.Start(processStartInfo))
+                    {
+                        process?.WaitForExit();
+                    }
+
+                    // Update the progress bar
+                    PBMain.Value++;
+                }
+
+                // Delete all temporary files after installing all packages
+                Directory.Delete(tempPath, true); // Deletes the folder and its contents
+            }
+            catch (Exception ex)
+            {
+                // Display error message
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Clean up temporary files if there's an issue
+                try
+                {
+                    if (Directory.Exists(tempPath))
+                    {
+                        Directory.Delete(tempPath, true); // Deletes the folder and its contents
+                    }
+                }
+                catch
+                {
+                    // Do nothing if there's an error deleting temporary files
+                }
+
+                // Close the application after the error
+                Application.Exit();
+            }
+
+            // Display completion message after all installations and before closing the program
+            MessageBox.Show("All installations have been successfully completed!", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Close the application
+            Application.Exit(); // Closes the entire application
+        }
+
+        private void btnInstallx64_Click(object sender, EventArgs e)
+        {
+            string tempPath = Path.Combine(Path.GetTempPath(), "CompInstall");
+            Directory.CreateDirectory(tempPath);
+
+            var installFiles = new (string fileName, string arguments)[]
+            {
+        ("2005x64.EXE", "/q"),
+        ("2008x64.exe", "/q"),
+        ("2010x64.exe", "/quiet /norestart"),
+        ("2012x64.exe", "/quiet /norestart"),
+        ("2013x64.exe", "/quiet /norestart"),
+        ("2022x64.exe", "/quiet /norestart"),
+        ("xnafx40_redist.msi", "/quiet /norestart")
+            };
+
+            PBMain.Maximum = installFiles.Length;
+
+            try
+            {
+                foreach (var (fileName, arguments) in installFiles)
+                {
+                    string filePath = Path.Combine(tempPath, fileName);
+
+                    // Extract the embedded file to the temporary directory
+                    using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"CompInstall.{fileName}"))
+                    using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                        resourceStream?.CopyTo(fileStream);
+
+                    // Run the installation
+                    var processStartInfo = new ProcessStartInfo(
+                        fileName.EndsWith(".msi") ? "msiexec.exe" : filePath,
+                        fileName.EndsWith(".msi") ? $"/i \"{filePath}\" {arguments}" : arguments)
+                    {
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                    };
+
+                    using (var process = Process.Start(processStartInfo))
+                    {
+                        process?.WaitForExit();
+                    }
+
+                    // Update the progress bar
+                    PBMain.Value++;
+                }
+
+                // Delete all temporary files after installing all packages
+                Directory.Delete(tempPath, true); // Deletes the folder and its contents
+            }
+            catch (Exception ex)
+            {
+                // Display error message
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Clean up temporary files if there's an issue
+                try
+                {
+                    if (Directory.Exists(tempPath))
+                    {
+                        Directory.Delete(tempPath, true); // Deletes the folder and its contents
+                    }
+                }
+                catch
+                {
+                    // Do nothing if there's an error deleting temporary files
+                }
+
+                // Close the application after the error
+                Application.Exit();
+            }
+
+            // Display completion message after all installations and before closing the program
+            MessageBox.Show("All installations have been successfully completed!", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Close the application
+            Application.Exit(); // Closes the entire application
+        }
+    }
+}
